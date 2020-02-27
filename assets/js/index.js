3900b2e31919f96d8b281a9b3394f4e729c2451e
Amira Mohamed <amira.mohamed@esrinea.local>
2020-02-27 15:34:26 +0200

    first commit

---
+++ assets/js/index.js
@@ -0,0 +1,16 @@
+$(document).ready(function(){
+  $('#sidebarContent').slimscroll({
+   height: '100%'
+  });
+
+  // $('.preview').slimscroll({
+  //   height: '100%'
+  //  });
+
+  $(".copy").click(function(){
+    $(".source-code").select();
+  });  
+  
+
+  
+  });
